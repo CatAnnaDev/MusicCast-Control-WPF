28c49b9c081c2367db4da100e42a1d23a29cd373
PsykoDev <johann.l_62@hotmail.fr>
2022-12-08 19:03:39 +0100

    Save and load profile

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,36 +1,28 @@
-﻿namespace MusicCast_Control_WPF;
+﻿using MusicCast_Control_WPF.SaveLoadProfile;
+using System.Windows.Input;
+
+namespace MusicCast_Control_WPF;
 
 public partial class MainWindow : Window
 {
     private ConfigBuild config = new();
     private SystemConfig systemconfig = new();
     private ZoneConfig zoneconfig = new();
+    private SaveLoadTemplate saveLoadTemplate = new();
     private string[] inputs = new string[50];
     private string[] sound_program = new string[50];
-
+    private string ProfileSelected;
     private string input_list;
     private string sound_program_list;
-    private string currentInput;
-    private string currentSoundProgram;
     private string maxVol;
-    private string curVol;
-    private bool mute;
-    private bool PureDirect;
-    private bool Enhancer;
-    private bool ExtraBass;
-    private bool Adaptivedrc;
     private int maxBass = 12;
     private int minBass = -12;
-    private int curBass;
     private int maxDialLevel = 3;
     private int minDialLevel = 0;
-    private int curDialLevel;
     private int tonetreblemax = 12;
     private int tonetreblemin = -12;
-    private int tonetreble;
     private int tonebassmax = 12;
     private int tonebassmin = -12;
-    private int tonebass;
 
     public MainWindow()
     {
@@ -40,13 +32,30 @@ public partial class MainWindow : Window
     private async void Window_Loaded(object sender, RoutedEventArgs e)
     {
         await config.InitializeAsync();
-        Setup();
+        Setup();      
     }
 
     private void Setup()
     {
         YamahaAV.ip = config.Config.IP;
         fetch_info();
+        ProfileCheck();
+    }
+
+    private async Task ProfileCheck()
+    {
+        if (Directory.Exists(Directory.GetCurrentDirectory() + "/Profile/"))
+        {
+            DirectoryInfo d = new DirectoryInfo(Directory.GetCurrentDirectory() + "/Profile/");
+            FileInfo[] Files = d.GetFiles("*.json");
+            string str = "";
+            foreach (FileInfo file in Files)
+            {
+                SavedProfile.Items.Add(file.Name);
+            }
+        }
+        else
+            Directory.CreateDirectory(Directory.GetCurrentDirectory() + "/Profile/");
     }
 
     private void Read_input_list()
@@ -58,7 +67,7 @@ public partial class MainWindow : Window
         }
 
         foreach (var input in inputs)
-            if (currentInput == input.ToLower())
+            if (saveLoadTemplate.currentInput == input.ToLower())
                 InputList.Text = input;
     }
 
@@ -71,7 +80,7 @@ public partial class MainWindow : Window
         }
 
         foreach (var input in sound_program)
-            if (currentSoundProgram == input.ToLower())
+            if (saveLoadTemplate.currentSoundProgram == input.ToLower())
                 SoundProgramList.Text = input;
     }
 
@@ -97,33 +106,33 @@ public partial class MainWindow : Window
             AmpliNameLabel.Content = (string)deviceinfo["model_name"];
 
             maxVol = Convert.ToString(status["max_volume"]);
-            curVol = Convert.ToString(status["volume"]);
-            currentInput = Convert.ToString(status["input"]);
-            currentSoundProgram = Convert.ToString(status["sound_program"]);
+            saveLoadTemplate.curVol = Convert.ToString(status["volume"]);
+            saveLoadTemplate.currentInput = Convert.ToString(status["input"]);
+            saveLoadTemplate.currentSoundProgram = Convert.ToString(status["sound_program"]);
 
-            mute = (bool)status["mute"];
-            PureDirect = (bool)status["pure_direct"];
-            Enhancer = (bool)status["enhancer"];
-            ExtraBass = (bool)status["extra_bass"];
-            Adaptivedrc = (bool)status["adaptive_drc"];
-            curDialLevel = int.Parse(Convert.ToString(status["dialogue_level"]));
-            curBass = int.Parse(Convert.ToString(status["subwoofer_volume"]));
-            tonetreble = int.Parse(Convert.ToString(status["tone_control"]["treble"]));
-            tonebass = int.Parse(Convert.ToString(status["tone_control"]["bass"]));
+            saveLoadTemplate.mute = (bool)status["mute"];
+            saveLoadTemplate.PureDirect = (bool)status["pure_direct"];
+            saveLoadTemplate.Enhancer = (bool)status["enhancer"];
+            saveLoadTemplate.ExtraBass = (bool)status["extra_bass"];
+            saveLoadTemplate.Adaptivedrc = (bool)status["adaptive_drc"];
+            saveLoadTemplate.curDialLevel = int.Parse(Convert.ToString(status["dialogue_level"]));
+            saveLoadTemplate.curBass = int.Parse(Convert.ToString(status["subwoofer_volume"]));
+            saveLoadTemplate.tonetreble = int.Parse(Convert.ToString(status["tone_control"]["treble"]));
+            saveLoadTemplate.tonebass = int.Parse(Convert.ToString(status["tone_control"]["bass"]));
 
-            PureDirectbutton.Content = $"Pure Direct: {PureDirect}";
-            EnhancerButton.Content = $"Enhancer: {Enhancer}";
-            AdaptiveDrcButton.Content = $"Adaptive drc: {Adaptivedrc}";
-            ExtraBassButton.Content = $"Extra Bass: {ExtraBass}";
-            DialogueLevelLabel.Content = $"Dial: {curDialLevel}";
-            BassLabel.Content = $"Bass: {curBass}";
-            ToneTrebleLabel.Content = $"Treble: {tonetreble}";
-            ToneBassLabel.Content = $"Bass: {tonebass}";
+            PureDirectbutton.Content = $"Pure Direct: {saveLoadTemplate.PureDirect}";
+            EnhancerButton.Content = $"Enhancer: {saveLoadTemplate.Enhancer}";
+            AdaptiveDrcButton.Content = $"Adaptive drc: {saveLoadTemplate.Adaptivedrc}";
+            ExtraBassButton.Content = $"Extra Bass: {saveLoadTemplate.ExtraBass}";
+            DialogueLevelLabel.Content = $"Dial: {saveLoadTemplate.curDialLevel}";
+            BassLabel.Content = $"Bass: {saveLoadTemplate.curBass}";
+            ToneTrebleLabel.Content = $"Treble: {saveLoadTemplate.tonetreble}";
+            ToneBassLabel.Content = $"Bass: {saveLoadTemplate.tonebass}";
 
             SoundFormatLabel.Content = $"format: {signalinfo["audio"]["format"]}\nfs: {signalinfo["audio"]["fs"]}";
 
 
-            if (mute)
+            if (saveLoadTemplate.mute)
                 SoundDBLabel.Content = "muted";
             else
                 SoundDBLabel.Content = Convert.ToString(status["actual_volume"]["value"]) + " dB";
@@ -156,60 +165,60 @@ public partial class MainWindow : Window
 
     private async void Mutebutton_Click(object sender, RoutedEventArgs e)
     {
-        if (!mute)
+        if (!saveLoadTemplate.mute)
         {
             await zoneconfig.setMute(true);
-            mute = true;
+            saveLoadTemplate.mute = true;
             SoundDBLabel.Content = "muted";
         }
         else
         {
             await zoneconfig.setMute(false);
-            mute = false;
+            saveLoadTemplate.mute = false;
         }
     }
 
     private async void VolUpButton_Click(object sender, RoutedEventArgs e)
     {
-        if (Convert.ToInt32(curVol) < Convert.ToInt32(maxVol))
+        if (Convert.ToInt32(saveLoadTemplate.curVol) < Convert.ToInt32(maxVol))
         {
-            var setVol = Convert.ToInt32(curVol) + 1;
+            var setVol = Convert.ToInt32(saveLoadTemplate.curVol) + 1;
 
             await zoneconfig.setVolume(setVol);
-            curVol = Convert.ToString(setVol);
-            mute = false;
+            saveLoadTemplate.curVol = Convert.ToString(setVol);
+            saveLoadTemplate.mute = false;
         }
     }
 
     private async void VolDownButton_Click(object sender, RoutedEventArgs e)
     {
-        if (Convert.ToInt32(curVol) > 0)
+        if (Convert.ToInt32(saveLoadTemplate.curVol) > 0)
         {
-            var setVol = Convert.ToInt32(curVol) - 1;
+            var setVol = Convert.ToInt32(saveLoadTemplate.curVol) - 1;
 
             await zoneconfig.setVolume(setVol);
-            curVol = Convert.ToString(setVol);
-            mute = false;
+            saveLoadTemplate.curVol = Convert.ToString(setVol);
+            saveLoadTemplate.mute = false;
         }
     }
 
     private async void InputList_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
         var selectedInput = ((string)InputList.SelectedItem).ToLower();
-        if (currentInput != selectedInput)
+        if (saveLoadTemplate.currentInput != selectedInput)
         {
             await zoneconfig.setInput(selectedInput);
-            currentInput = selectedInput;
+            saveLoadTemplate.currentInput = selectedInput;
         }
     }
 
     private async void SoundProgramList_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
         var selectedInput = ((string)SoundProgramList.SelectedItem).ToLower();
-        if (currentSoundProgram != selectedInput)
+        if (saveLoadTemplate.currentSoundProgram != selectedInput)
         {
             await zoneconfig.setSoundProgram(selectedInput);
-            currentSoundProgram = selectedInput;
+            saveLoadTemplate.currentSoundProgram = selectedInput;
         }
     }
 
@@ -222,114 +231,184 @@ public partial class MainWindow : Window
 
     private async void BassDown_Click(object sender, RoutedEventArgs e)
     {
-        if (curBass >= minBass || curBass <= maxBass)
+        if (saveLoadTemplate.curBass >= minBass || saveLoadTemplate.curBass <= maxBass)
         {
-            curBass = curBass - 1;
-            await zoneconfig.setSubwooferVolume(volume: curBass);
+            saveLoadTemplate.curBass = saveLoadTemplate.curBass - 1;
+            await zoneconfig.setSubwooferVolume(volume: saveLoadTemplate.curBass);
         }
     }
 
     private async void BassUp_Click(object sender, RoutedEventArgs e)
     {
-        if (curBass >= minBass || curBass <= maxBass)
+        if (saveLoadTemplate.curBass >= minBass || saveLoadTemplate.curBass <= maxBass)
         {
-            curBass = curBass + 1;
-            await zoneconfig.setSubwooferVolume(volume: curBass);
+            saveLoadTemplate.curBass = saveLoadTemplate.curBass + 1;
+            await zoneconfig.setSubwooferVolume(volume: saveLoadTemplate.curBass);
         }
     }
 
     private async void dialDown_Click(object sender, RoutedEventArgs e)
     {
-        if (curDialLevel >= minDialLevel || curDialLevel <= maxDialLevel)
+        if (saveLoadTemplate.curDialLevel >= minDialLevel || saveLoadTemplate.curDialLevel <= maxDialLevel)
         {
-            curDialLevel = curDialLevel - 1;
-            await zoneconfig.setDialogueLevel(value: curDialLevel);
+            saveLoadTemplate.curDialLevel = saveLoadTemplate.curDialLevel - 1;
+            await zoneconfig.setDialogueLevel(value: saveLoadTemplate.curDialLevel);
         }
     }
 
     private async void DialUp_Click(object sender, RoutedEventArgs e)
     {
-        if (curDialLevel >= minDialLevel || curDialLevel <= maxDialLevel)
+        if (saveLoadTemplate.curDialLevel >= minDialLevel || saveLoadTemplate.curDialLevel <= maxDialLevel)
         {
-            curDialLevel = curDialLevel + 1;
-            await zoneconfig.setDialogueLevel(value: curDialLevel);
+            saveLoadTemplate.curDialLevel = saveLoadTemplate.curDialLevel + 1;
+            await zoneconfig.setDialogueLevel(value: saveLoadTemplate.curDialLevel);
         }
     }
 
     private async void ToneBassDown_Click(object sender, RoutedEventArgs e)
     {
-        if (tonebass >= tonebassmin || tonebass <= tonebassmax)
+        if (saveLoadTemplate.tonebass >= tonebassmin || saveLoadTemplate.tonebass <= tonebassmax)
         {
-            tonebass = tonebass - 1;
-            await zoneconfig.setToneControl(null, tonebass);
+            saveLoadTemplate.tonebass = saveLoadTemplate.tonebass - 1;
+            await zoneconfig.setToneControl(null, saveLoadTemplate.tonebass);
         }
     }
 
     private async void ToneBassUp_Click(object sender, RoutedEventArgs e)
     {
-        if (tonebass >= tonebassmin || tonebass <= tonebassmax)
+        if (saveLoadTemplate.tonebass >= tonebassmin || saveLoadTemplate.tonebass <= tonebassmax)
         {
-            tonebass = tonebass + 1;
-            await zoneconfig.setToneControl(null, tonebass);
+            saveLoadTemplate.tonebass = saveLoadTemplate.tonebass + 1;
+            await zoneconfig.setToneControl(null, saveLoadTemplate.tonebass);
         }
     }
 
     private async void ToneTrebleDown_Click(object sender, RoutedEventArgs e)
     {
-        if (tonetreble >= tonetreblemin || tonetreble <= tonetreblemax)
+        if (saveLoadTemplate.tonetreble >= tonetreblemin || saveLoadTemplate.tonetreble <= tonetreblemax)
         {
-            tonetreble = tonetreble - 1;
-            await zoneconfig.setToneControl(tonetreble, null);
+            saveLoadTemplate.tonetreble = saveLoadTemplate.tonetreble - 1;
+            await zoneconfig.setToneControl(saveLoadTemplate.tonetreble, null);
         }
     }
 
     private async void ToneTrebleUp_Click(object sender, RoutedEventArgs e)
     {
-        if (tonetreble >= tonetreblemin || tonetreble <= tonetreblemax)
+        if (saveLoadTemplate.tonetreble >= tonetreblemin || saveLoadTemplate.tonetreble <= tonetreblemax)
         {
-            tonetreble = tonetreble + 1;
-            await zoneconfig.setToneControl(tonetreble, null);
+            saveLoadTemplate.tonetreble = saveLoadTemplate.tonetreble + 1;
+            await zoneconfig.setToneControl(saveLoadTemplate.tonetreble, null);
         }
     }
 
     private async void PureDirectbutton_Click(object sender, RoutedEventArgs e)
     {
-        if (!PureDirect)
+        if (!saveLoadTemplate.PureDirect)
             await zoneconfig.setPureDirect(true);
         else
             await zoneconfig.setPureDirect(false);
-        PureDirectbutton.Content = $"Pure Direct: {PureDirect}";
+        PureDirectbutton.Content = $"Pure Direct: {saveLoadTemplate.PureDirect}";
     }
 
     private async void EnhancerButton_Click(object sender, RoutedEventArgs e)
     {
-        if (!Enhancer)
+        if (!saveLoadTemplate.Enhancer)
             await zoneconfig.setEnhancer(true);
         else
             await zoneconfig.setEnhancer(false);
-        EnhancerButton.Content = $"Enhancer: {Enhancer}";
+        EnhancerButton.Content = $"Enhancer: {saveLoadTemplate.Enhancer}";
     }
 
     private async void AdaptiveDrcButton_Click(object sender, RoutedEventArgs e)
     {
-        if (!Adaptivedrc)
+        if (!saveLoadTemplate.Adaptivedrc)
             await zoneconfig.setAdaptiveDrc(true);
         else
             await zoneconfig.setAdaptiveDrc(false);
-        AdaptiveDrcButton.Content = $"Adaptive drc: {Adaptivedrc}";
+        AdaptiveDrcButton.Content = $"Adaptive drc: {saveLoadTemplate.Adaptivedrc}";
     }
 
     private async void ExtraBassButton_Click(object sender, RoutedEventArgs e)
     {
-        if (!ExtraBass)
+        if (!saveLoadTemplate.ExtraBass)
             await zoneconfig.setExtraBass(true);
         else
             await zoneconfig.setExtraBass(false);
-        ExtraBassButton.Content = $"Extra Bass: {ExtraBass}";
+        ExtraBassButton.Content = $"Extra Bass: {saveLoadTemplate.ExtraBass}";
     }
 
     private async void PureDirectbutton1_Click(object sender, RoutedEventArgs e)
     {
-
+        // idk for now
     }
+
+    private async void Save_Click(object sender, RoutedEventArgs e)
+    {
+        var Profile1 = new SaveLoadTemplate
+        {
+            currentInput = saveLoadTemplate.currentInput,
+            currentSoundProgram = saveLoadTemplate.currentSoundProgram,
+            curVol = saveLoadTemplate.curVol,
+            curBass = saveLoadTemplate.curBass,
+            curDialLevel = saveLoadTemplate.curDialLevel,
+            tonetreble = saveLoadTemplate.tonetreble,
+            tonebass = saveLoadTemplate.tonebass,
+            mute = saveLoadTemplate.mute,
+            PureDirect = saveLoadTemplate.PureDirect,
+            Enhancer = saveLoadTemplate.Enhancer,
+            ExtraBass = saveLoadTemplate.ExtraBass,
+            Adaptivedrc = saveLoadTemplate.Adaptivedrc
+        };
+
+        if (ProfileSelected == null)
+            ProfileSelected = "Profile1.json";
+
+        string fileName = $"Profile/{ProfileSelected}";
+        var save = JsonConvert.SerializeObject(Profile1, Formatting.Indented);
+        File.WriteAllText(fileName, save);
+
+        await ProfileCheck();
+    }
+
+    private async void Load_Click(object sender, RoutedEventArgs e)
+    {
+        string fileName = $"Profile/{ProfileSelected}";
+        string jsonString = File.ReadAllText(fileName);
+        var load = JsonConvert.DeserializeObject<SaveLoadTemplate>(jsonString);
+
+        if(saveLoadTemplate.currentInput != load.currentInput)
+            await zoneconfig.setInput(load.currentInput);
+        if (saveLoadTemplate.currentSoundProgram != load.currentSoundProgram)
+            await zoneconfig.setSoundProgram(load.currentSoundProgram);
+        if (saveLoadTemplate.curVol != load.curVol)
+            await zoneconfig.setVolume(Convert.ToInt32(load.curVol));
+        if (saveLoadTemplate.curBass != load.curBass)
+            await zoneconfig.setSubwooferVolume(volume: load.curBass);
+        if (saveLoadTemplate.curDialLevel != load.curDialLevel)
+            await zoneconfig.setDialogueLevel(value: load.curDialLevel);
+        if (saveLoadTemplate.tonetreble != load.tonetreble)
+            await zoneconfig.setToneControl(load.tonetreble, null);
+        if (saveLoadTemplate.tonebass != load.tonebass)
+            await zoneconfig.setToneControl(null, load.tonebass);
+        if (saveLoadTemplate.mute != load.mute)
+            await zoneconfig.setMute(load.mute);
+        if (saveLoadTemplate.PureDirect != load.PureDirect)
+            await zoneconfig.setPureDirect(load.PureDirect);
+        if (saveLoadTemplate.Enhancer != load.Enhancer)
+            await zoneconfig.setEnhancer(load.Enhancer);
+        if (saveLoadTemplate.ExtraBass != load.ExtraBass)
+            await zoneconfig.setExtraBass(load.ExtraBass);
+        if (saveLoadTemplate.Adaptivedrc != load.Adaptivedrc)
+            await zoneconfig.setAdaptiveDrc(load.Adaptivedrc);
+
+        foreach (var input in inputs)
+            if (saveLoadTemplate.currentInput == input.ToLower())
+                InputList.Text = input;
+
+        foreach (var input in sound_program)
+            if (saveLoadTemplate.currentSoundProgram == input.ToLower())
+                SoundProgramList.Text = input;
+    }
+
+    private void SavedProfile_SelectionChanged(object sender, SelectionChangedEventArgs e) => ProfileSelected = SavedProfile.SelectedItem.ToString();
 }
---
+++ SaveLoadProfile/SaveLoadTemplate.cs
@@ -0,0 +1,17 @@
+﻿namespace MusicCast_Control_WPF.SaveLoadProfile;
+
+internal class SaveLoadTemplate
+{
+  public string currentInput {get;set;}
+  public string currentSoundProgram {get;set;}
+  public string curVol {get;set;}
+  public int curBass {get;set;}
+  public int curDialLevel {get;set;}
+  public int tonetreble {get;set;}
+  public int tonebass {get;set;}
+  public bool mute {get;set;}
+  public bool PureDirect {get;set;}
+  public bool Enhancer {get;set;}
+  public bool ExtraBass {get;set;}
+  public bool Adaptivedrc { get; set; }
+}
